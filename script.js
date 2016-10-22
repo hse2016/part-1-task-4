27a88d2feaa3c58dfdf0c0ce44f2c289132f6c31
Nitori Kawashiro <nitori.kawashiro.sync@gmail.com>
2016-10-22 22:13:26 +0300

    Added script

---
+++ script.js
@@ -0,0 +1,45 @@
+let leftCanvas = document.getElementById('leftCanvas');
+let rightCanvas = document.getElementById('rightCanvas');
+
+ini(leftCanvas, rightCanvas);
+drawGrid(leftCanvas, 10);
+drawGrid(rightCanvas, 10);
+
+function ini(firstCanvas, secondCanvas) {
+  let leftDecorator = document.getElementById('leftDecorator');
+  let rightDecorator = document.getElementById('rightDecorator');
+
+  let leftCanvasWidth = leftDecorator.offsetWidth;
+  let leftCanvasHeight = leftDecorator.offsetHeight;
+  let rightCanvasWidth = rightDecorator.offsetWidth;
+  let rightCanvasHeight = rightDecorator.offsetHeight;
+
+  // dich
+  firstCanvas.width = leftCanvasWidth;
+  firstCanvas.height = leftCanvasHeight;
+  secondCanvas.width = rightCanvasWidth;
+  secondCanvas.height = rightCanvasHeight;
+}
+
+function drawGrid(canvas, diff) {
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    let count = Math.round((canvas.width + canvas.height) / diff);
+    let x1;
+    let y1;
+    let x2;
+    let y2;
+
+    for (let i = 0; i < count; ++i) {
+      x1 = 0;
+      y1 = -canvas.width + i * diff;
+
+      x2 = canvas.width;
+      y2 = -canvas.width + i * diff + canvas.width;
+
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+    }
+    ctx.stroke();
+  }
+}
