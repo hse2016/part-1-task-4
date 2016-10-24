23746b967bd770dd7c91cbdb9ae3f8899f1bbb52
WERT-PC <sv-wert@yandex.ru>
2016-10-24 23:58:08 +0300

    Сделал, чтобы затемнение вокруг окна было всегда на весь экран (раньше, если уменьшить масштаб, то оно обрывалось снизу). Если в окне оставить меньше информации, то оно будет открываться по центру (правда, если перед этим не промотать страницу вниз).

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var popupBackground = document.getElementById('popup-background');
+var pupupWrapper = document.getElementById('popup-wrapper-table');
 
 function showPopup() {
     popupBackground.style.display='block';
+    pupupWrapper.style.display='table';
 }
 
 function closePopup() {
     popupBackground.style.display='none';
+    pupupWrapper.style.display='none';
 }
